36f930ce41e86a19fd5798a4c942ebf12bdf07d6
danimal141 <hideaki.ishii1204@gmail.com>
2023-05-06 15:32:44 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Installing Xcode..."
+xcode-select --install
+
+# Install rosetta
+# echo "Installing rosetta..."
+# sudo softwareupdate --install-rosetta --agree-to-licensesudo softwareupdate --install-rosetta --agree-to-license
+
+#------------------------------------------
+# homebrew (arm64)
+#------------------------------------------
+echo "Installing homebrew..."
+which /opt/homebrew/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Execute brew doctor..."
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew doctor
+
+echo "Execute brew update..."
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew update --verbose
+
+echo "Execute brew upgrade..."
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew upgrade --verbose
+
+echo "Installing packages written in Brewfile..."
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew bundle --file ./.Brewfile --verbose
+
+echo "Execute brew cleanup..."
+which brew >/dev/null 2>&1 && brew cleanup --verbose
+
+echo "Installing programming languages..."
+./_asdf.sh
+
+echo "Symlink dotfiles..."
+homesick link dotfiles
+
+# Replace the current shell with a new shell, run it as a login shell, and reset the environment settings.
+exec $SHELL -l
